1f13d44e587907a0704970384d12d18a98631828
enzoftware <lizama.enzo@gmail.com>
2019-03-27 00:07:46 -0500

    Add the models for the project

---
+++ lib/domain/model/models.dart
@@ -0,0 +1,56 @@
+class JobEntity {
+  String id;
+  String type;
+  String url;
+  String createdAt;
+  String company;
+  String location;
+  String companyUrl;
+  String title;
+  String description;
+  String companyLogo;
+  String howToApply;
+
+  JobEntity(
+      {this.id,
+      this.type,
+      this.url,
+      this.createdAt,
+      this.company,
+      this.location,
+      this.companyUrl,
+      this.title,
+      this.description,
+      this.companyLogo,
+      this.howToApply});
+
+  JobEntity.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    type = json['type'];
+    url = json['url'];
+    createdAt = json['created_at'];
+    company = json['company'];
+    companyUrl = json['company_url'];
+    location = json['location'];
+    title = json['title'];
+    description = json['description'];
+    howToApply = json['how_to_apply'];
+    companyLogo = json['company_logo'] ?? '';
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['type'] = this.type;
+    data['url'] = this.url;
+    data['created_at'] = this.createdAt;
+    data['company'] = this.company;
+    data['company_url'] = this.companyUrl;
+    data['location'] = this.location;
+    data['title'] = this.title;
+    data['description'] = this.description;
+    data['how_to_apply'] = this.howToApply;
+    data['company_logo'] = this.companyLogo;
+    return data;
+  }
+}
