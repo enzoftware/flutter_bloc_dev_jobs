384625a39ff6ea10972ed23af3a374ba7b0adeff
enzoftware <lizama.enzo@gmail.com>
2019-03-29 08:07:11 -0500

    Base ui for widgets

---
+++ lib/ui/job_card.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:dev_jobs_bloc/domain/model/models.dart';
+
+class JobCard extends StatelessWidget {
+  final JobEntity jobEntity;
+
+  JobCard({Key key, this.jobEntity}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+  Widget _buildIcon() {
+    return Container();
+  }
+}
