961a7367fd699162dee0c44d0d0e679e6ab4c2a6
Enzo Lizama <lizama.enzo@gmail.com>
2019-04-02 19:46:22 -0500

    Base of app home page

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,5 +1,8 @@
+import 'package:dev_jobs_bloc/bloc/github_jobs_bloc.dart';
+import 'package:dev_jobs_bloc/domain/repository/github_jobs_repository.dart';
 import 'package:flutter/material.dart';
-import 'package:dev_jobs_bloc/ui/job_card.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,8 +10,36 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  HomePageBloc _homePageBloc;
+  TextEditingController _textEditingController;
+
+  double searchElevation = 1;
+
+
+  @override
+  void initState() {
+    super.initState();
+    _textEditingController = TextEditingController();
+    _homePageBloc = HomePageBloc(
+        repository: GithubJobsRepository()
+    );
+  }
+
+
+  @override
+  void dispose() {
+    super.dispose();
+    _homePageBloc.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return BlocProvider<HomePageBloc>(
+      bloc: _homePageBloc,
+      child: Scaffold(
+        body: Stack(),
+      ),
+    );
   }
 }
